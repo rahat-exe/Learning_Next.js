588424f13da82cf162b21744f1d7c56c0c1e42dd
Rahat <rrahatt.exe@gmail.com>
2026-01-30 20:25:25 +0530

    completed cookies

--- 5_backend_routes/app/api/cookies/route.js
+++ 5_backend_routes/app/api/cookies/route.js
@@ -1,14 +1,28 @@
 import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
-export async function GET(request){
+export async function GET(request) {
+  //read cookies from the request
+  //   const theme = request.cookies.get("theme");
+  //   console.log(theme);
 
-    //read cookies from the request
-    const theme = request.cookies.get("theme")
-    console.log(theme)
+  const cookieStore = await cookies();
 
-    return new Response("Setting cookies",{
-        headers:{
-            "Set-Cookie":"theme=dart"
-        }
-    })
+  const myCookies = cookieStore.get("theme");
+  // console.log(myCookies)
+
+  const score = cookieStore.get("score");
+  console.log(score);
+
+  cookieStore.set("score", "100");
+
+  //   return new Response("Setting cookies", {
+  //     headers: {
+  //       "Set-Cookie": "theme=dark",
+  //     },
+  //   });
+
+  return NextResponse.json({ message: "cookies set!" });
 }
+
+// Cookies are small key–value data stored by the browser and automatically sent to the server with every request.
