733b78979f93aed892663d978479d53474131e37
Rahat <rrahatt.exe@gmail.com>
2026-02-11 23:29:01 +0530

    fixed something

--- 12_todo_app/store/todoStore.js
+++ 12_todo_app/store/todoStore.js
@@ -3,21 +3,22 @@ import {devtools} from "zustand/middleware"
 
 
 export const useTodoStore = create(
-    devtools(
-        (set, get) => ({
-            todos: [],
-            filter:"all",
-            isLoading: false,
+  devtools(
+    (set, get) => ({
+      todos: [],
+      filter: "all",
+      isLoading: false,
 
-            setTodos: (todos) => set({todos}),
+      setTodos: (todos) => set({ todos }),
+      // setTodos: (newTodos) => set({ todos: newTodos }),
 
-            addTodo : (todo) =>
-                set((state)=> ({
-                    todos:[todo, ...state.todos]
-                }))
-        }),
-        {
-            name:"todo-store"
-        }
-    )
-)
+      addTodo: (todo) =>
+        set((state) => ({
+          todos: [todo, ...state.todos],
+        })),
+    }),
+    {
+      name: "todo-store",
+    },
+  ),
+);
