337d78f675a718657b7899828166cec4e952ba23
Rahat <rrahatislamm@gmail.com>
2026-02-27 01:31:33 +0530

    setup better-auth

---
+++ 16_auth/lib/auth.js
@@ -0,0 +1,23 @@
+import { betterAuth } from "better-auth";
+import {prismaAdapter} from "better-auth/adapters/prisma"
+import prisma from "./db";
+
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    Provider: "postgesql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    },
+  },
+});
